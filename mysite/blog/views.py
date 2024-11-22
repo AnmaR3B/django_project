83cefc15b2dd58ef31ff48bca308579335c2c748
= <anmarma83@gmail.com>
2024-11-22 09:28:05 +0500

    building class-based views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,19 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger 
+from django.views.generic import ListView
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     paginator = Paginator(post_list,3)
+#     page_number = request.GET.get('page',1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger: 
+#         posts =  paginator.page(1) 
 
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list,3)
-    page_number = request.GET.get('page',1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger: 
-        posts =  paginator.page(1) 
+#     return render(request, 'blog/post/list.html', {'posts':posts}) # sending context for template list.html
 
-    return render(request, 'blog/post/list.html', {'posts':posts})
+class PostListView(ListView):
+    """
+    Alternative post list view """
+    model = Post
+    content_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year,month,day,post):
 #def post_detail(request, id):
